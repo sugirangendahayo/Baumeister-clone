66e5279a1c0244513e49b52c08e7557554e40a9d
Victor <ansimavicky@gmail.com>
2025-12-11 11:17:10 +0200

    feat : sticked the red vector below the nav bar

--- src/main.js
+++ src/main.js
@@ -1,7 +1,3 @@
-//Translation library imports 
-// import i18next from "i18next";
-// import deTranslation from "./i18netTranslation/DE.json";
-// import enTranslation from "./i18netTranslation/EN.json";
 
 
 //Crolling to hash EveryTime
@@ -14,33 +10,3 @@ if (hash) {
         }
     }, 70)
 }
-
-
-
-
-//function responsible for translation 
-// const translatePageOnDom = () => {
-//     document.querySelectorAll("[data-i18n]").forEach(el => {
-//         const key = el.getAttribute("data-i18n");
-//         el.key.textContent = i18next.t(key);
-//     })
-// }
-// //initial language setup
-// const setUp = async () => {
-//     await i18next.init({
-//         lng: "en",
-//         resources: {
-//             en: { translation: enTranslation },
-//             de: { translation: deTranslation }
-//         },
-//         defaultNS: "translation",
-//         fallbackLng: "en",
-//     })
-// }
-
-// translatePageOnDom();
-
-
-// // if (setToDutch, setToEnglish) {
-//  console.log("hdhdhdh")
-// // }
